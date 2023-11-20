bc079f55df6f9b6c28f8885100d50c10d58ee797
SiarheiKazakevich <dianrom@tut.by>
2023-11-20 23:18:43 +0300

    feat: checked finished 16th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -310,8 +310,11 @@ function orderAlphabetically(str) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  if (typeof str === 'string' && str !== null) {
+    return str.includes(substring);
+  }
+  return 0;
 }
 
 /**
